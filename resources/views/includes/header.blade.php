5cae271258c7da440f79377a91759472c1daa853
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-10-23 13:30:57 +0800

    Initial commit. No Laradock; not needed.

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<div class="nav navbar-main">
+  <a id="logo" class="navbar-brand" href="/">myMulti</a>
+  <ul class="nav">
+      <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+      <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+      <li class="nav-item"><a href="/projects" class="nav-link">Projects</a></li>
+      <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
+  </ul>
+</div>
