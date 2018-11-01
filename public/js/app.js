c50ca0c238437e797e98228fded836f80c89403e
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-11-02 05:15:32 +0800

    added media query for hieght for cellphones

--- public/js/app.js
+++ public/js/app.js
@@ -36072,54 +36072,10 @@ $(window).on('load', function () {
     frame = $("iframe#chat-" + channel)[0];
     $('#chat-button-' + channel).addClass('main');
 
-    // frame.setAttribute('height',height);
-    // frame.setAttribute('width',width);
     frame.setAttribute('class', 'active');
     $(frame).parent().addClass('active');
   });
 });
-
-// if ($(window).width() <= 900) {
-//   big_screen = true;
-// } else {
-//   big_screen = false;
-// }
-
-// $(window).on("resize", function() {
-//   height = 500;
-//   width = 400;
-//   main = $('.chat-channel iframe.main')[0];
-//
-//   if ($(window).width() <= 900) {
-//     if (big_screen == true) {
-//       to_resize = true;
-//       big_screen = false;
-//     };
-//   } else {
-//     if (big_screen == false) {
-//       to_resize = true;
-//       big_screen = false;
-//     }
-//   }
-//
-//   if (to_resize == true) {
-//     height = 500;
-//     if (big_screen == true) {
-//       width = 400;
-//     } else {
-//       width = 300;
-//     }
-//   }
-//
-//   if (to_resize == true ) {
-//     all_chat_channels.each( function() {
-//       ( this ).setAttribute('height', height);
-//       ( this ).setAttribute('width', width);
-//     });
-//     to_resize = false;
-//   }
-//
-// });
 
 /***/ }),
 /* 39 */
