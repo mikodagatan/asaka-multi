12061e760ebfe8655e92f38cad76f73e12528fab
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-11-05 18:35:36 +0800

    small front page changes

--- resources/views/pages/front.blade.php
+++ resources/views/pages/front.blade.php
@@ -4,7 +4,7 @@
   <div class="front">
     <div class="introductions">
       <h1>Welcome to AsakaMulti</h1>
-      <p>Hello everyone! I'm PhantasmaAsaka, and I'm both a Twitch streamer and a small-streamer supporter!</p>
+      <p>Hello everyone! I'm PhantasmaAsaka, and I'm both a small Twitch streamer and a small-streamer supporter!</p>
       <p>I made this website because I felt that the current available tools are insufficient to satisfy my needs when supporting other channels.</p>
       <p>Other people may feel the same, so I'm sharing this to all of you to use this to support the streamers you love!</p>
       <p>To start using this website, just type the channel names separated by a forward-slash (/).</p>
@@ -15,7 +15,7 @@
         <li>It makes sure your lurks are counted!</li>
         <li>It allows you to use your phone with a more (not best) responsive layout!</li>
       </ul>
-      <p>Note: Counted views need to have the videos unmuted. I minimized the volume for you guys already! However, if you want to mute the sounds, just mute the tab by right-clicking on it!</p>
+      <p>Note: Counted views need to have the videos unmuted. I minimized the volume for you guys already! However, if you want to mute the sounds, just mute the tab by right-clicking on it! Also, make sure that this website is on a separate window!</p>
       <p>If you like this project and want me to continue improving this website, you can donate through my twitch.tv stream! You can go to my website through the link below!</p>
     </div>
     <div class="ph-channel">
