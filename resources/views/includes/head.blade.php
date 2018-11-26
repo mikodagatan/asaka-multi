29a1213da35071293b26dcdcb62755f97065c7bd
Miguel Alberto U. Dagatan <miguel.dagatan@gmail.com>
2018-11-26 21:46:10 +0800

    added google analytics

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -8,4 +8,14 @@
   <script src="{{ mix('/js/app.js') }}"></script>
   <script src= "https://player.twitch.tv/js/embed/v1.js"></script>
 
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129676471-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-129676471-1');
+  </script>
+
+
   <title>AsakaMulti - Can Mobile! @yield('title')</title>
